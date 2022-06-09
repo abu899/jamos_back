ba0414d120c57190bab6b6f0ec0a096b0d876047
Ahn <abu89@naver.com>
2022-06-09 22:18:06 +0900

    [JamOS] Add Interceptor for SameSite
    + CookieInterceptor
      + SameSite for Chrome

--- src/main/java/jamos/back/WebConfig.java
+++ src/main/java/jamos/back/WebConfig.java
@@ -1,7 +1,9 @@
 package jamos.back;
 
+import jamos.back.global.exception.interceptor.CookieInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,6 +14,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://drawing-paper.netlify.app", "http://localhost:8080")
                 .allowCredentials(true)
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie")
                 .allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CookieInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/css/**", "/*.ico", "/error");
     }
 }
---
+++ src/main/java/jamos/back/global/exception/interceptor/CookieInterceptor.java
@@ -0,0 +1,31 @@
+package jamos.back.global.exception.interceptor;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+public class CookieInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        addSameSite(response, "None");
+        return true;
+    }
+
+    private void addSameSite(HttpServletResponse response, String sameSite) {
+        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
+        boolean isFirstHeader = true;
+        for (String header : headers) {
+            if (isFirstHeader) {
+                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s;Secure;%s",header, "SameSite="+sameSite));
+                isFirstHeader = false;
+                continue;
+            }
+            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s;Secure;%s",header, "SameSite="+sameSite));
+        }
+    }
+}
